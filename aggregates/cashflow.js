ade2908dab0ce06efe67adb6d22609543e17831c
Laba Subagia <labasubagia22@gmail.com>
2021-12-16 07:18:02 +0800

    refactor aggregation pipeline cashflow

---
+++ aggregates/cashflow.js
@@ -0,0 +1,125 @@
+const moment = require("moment");
+const { Types } = require("mongoose");
+const CategoryConfig = require("../config/category");
+
+const CashFlowPipeline = {
+  byUserSummary({
+    categoryModel,
+    user_id,
+    start_date = moment().startOf("month"),
+    end_date = moment().endOf("month"),
+  }) {
+    const startDate = moment(start_date).startOf("day").toDate();
+    const endDate = moment(end_date).endOf("day").toDate();
+    return [
+      { $match: { user_id: Types.ObjectId(user_id) } },
+      {
+        $lookup: {
+          from: categoryModel.collection.name,
+          localField: "category_id",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+      { $unwind: { path: "$category" } },
+      {
+        $addFields: {
+          category_name: "$category.name",
+          category_type: "$category.type",
+          category: "$$REMOVE",
+          amount_value: {
+            $cond: [
+              { $eq: ["$category.type", CategoryConfig.INCOME] },
+              "$amount",
+              { $subtract: [0, "$amount"] },
+            ],
+          },
+        },
+      },
+      {
+        $facet: {
+          all: [],
+          all_date_range: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $gte: ["$date", startDate] },
+                    { $lte: ["$date", endDate] },
+                  ],
+                },
+              },
+            },
+          ],
+          expense_date_range: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $gte: ["$date", startDate] },
+                    { $lte: ["$date", endDate] },
+                    { $eq: ["$category_type", CategoryConfig.EXPENSE] },
+                  ],
+                },
+              },
+            },
+          ],
+          income_date_range: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $gte: ["$date", startDate] },
+                    { $lte: ["$date", endDate] },
+                    { $eq: ["$category_type", CategoryConfig.INCOME] },
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          all: "$$REMOVE",
+          all_date_range: "$$REMOVE",
+          balance: { $sum: "$all.amount_value" },
+          balance_date_range: {
+            $sum: "$all_date_range.amount_value",
+          },
+          expense_date_range: {
+            $abs: { $sum: "$expense_date_range.amount_value" },
+          },
+          income_date_range: {
+            $sum: "$income_date_range.amount_value",
+          },
+        },
+      },
+    ];
+  },
+
+  byUserWithCategory({ categoryModel, user_id }) {
+    return [
+      { $match: { user_id: Types.ObjectId(user_id) } },
+      {
+        $lookup: {
+          from: categoryModel.collection.name,
+          localField: "category_id",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+      { $unwind: { path: "$category" } },
+      {
+        $addFields: {
+          category_name: "$category.name",
+          category_type: "$category.type",
+          category: "$$REMOVE",
+        },
+      },
+      { $sort: { created_at: -1, name: 1 } },
+    ];
+  },
+};
+
+module.exports = CashFlowPipeline;
--- services/cashflow.js
+++ services/cashflow.js
@@ -1,7 +1,6 @@
 const _ = require("lodash");
-const moment = require("moment");
 const { Types } = require("mongoose");
-const CategoryConfig = require("../config/category");
+const CashFlowPipeline = require("../aggregates/cashflow");
 
 class CashFlowService {
   constructor({ cashFlowModel, categoryModel }) {
@@ -9,125 +8,24 @@ class CashFlowService {
     this.categoryModel = categoryModel;
   }
 
-  async getSummary({
-    user_id,
-    start_date = moment().startOf("month"),
-    end_date = moment().endOf("month"),
-  }) {
-    const startDate = moment(start_date).startOf("day").toDate();
-    const endDate = moment(end_date).endOf("day").toDate();
-    const pipeline = [
-      { $match: { user_id: Types.ObjectId(user_id) } },
-      {
-        $lookup: {
-          from: this.categoryModel.collection.name,
-          localField: "category_id",
-          foreignField: "_id",
-          as: "category",
-        },
-      },
-      { $unwind: { path: "$category" } },
-      {
-        $addFields: {
-          category_name: "$category.name",
-          category_type: "$category.type",
-          category: "$$REMOVE",
-          amount_value: {
-            $cond: [
-              { $eq: ["$category.type", CategoryConfig.INCOME] },
-              "$amount",
-              { $subtract: [0, "$amount"] },
-            ],
-          },
-        },
-      },
-      {
-        $facet: {
-          all: [],
-          all_date_range: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $gte: ["$date", startDate] },
-                    { $lte: ["$date", endDate] },
-                  ],
-                },
-              },
-            },
-          ],
-          expense_date_range: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $gte: ["$date", startDate] },
-                    { $lte: ["$date", endDate] },
-                    { $eq: ["$category_type", CategoryConfig.EXPENSE] },
-                  ],
-                },
-              },
-            },
-          ],
-          income_date_range: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $gte: ["$date", startDate] },
-                    { $lte: ["$date", endDate] },
-                    { $eq: ["$category_type", CategoryConfig.INCOME] },
-                  ],
-                },
-              },
-            },
-          ],
-        },
-      },
-      {
-        $addFields: {
-          all: "$$REMOVE",
-          all_date_range: "$$REMOVE",
-          balance: { $sum: "$all.amount_value" },
-          balance_date_range: {
-            $sum: "$all_date_range.amount_value",
-          },
-          expense_date_range: {
-            $abs: { $sum: "$expense_date_range.amount_value" },
-          },
-          income_date_range: {
-            $sum: "$income_date_range.amount_value",
-          },
-        },
-      },
-    ];
+  async getSummary({ user_id, start_date, end_date }) {
+    const pipeline = CashFlowPipeline.byUserSummary({
+      categoryModel: this.categoryModel,
+      user_id,
+      start_date,
+      end_date,
+    });
     const data = await this.cashFlowModel
       .aggregate(pipeline)
       .allowDiskUse(true);
     return _.first(data);
   }
 
-  async getByUser(userId) {
-    const pipeline = [
-      { $match: { user_id: Types.ObjectId(userId) } },
-      {
-        $lookup: {
-          from: this.categoryModel.collection.name,
-          localField: "category_id",
-          foreignField: "_id",
-          as: "category",
-        },
-      },
-      { $unwind: { path: "$category" } },
-      {
-        $addFields: {
-          category_name: "$category.name",
-          category_type: "$category.type",
-          category: "$$REMOVE",
-        },
-      },
-      { $sort: { created_at: -1, name: 1 } },
-    ];
+  async getByUser(user_id) {
+    const pipeline = CashFlowPipeline.byUserWithCategory({
+      categoryModel: this.categoryModel,
+      user_id,
+    });
     return this.cashFlowModel.aggregate(pipeline).allowDiskUse(true);
   }
 
